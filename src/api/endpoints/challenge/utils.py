c0f31c76e2789a68a31f907e7b67e5f318583d87
aliyuldashev <javohir.hbai@gmail.com>
2026-02-23 08:15:04 +0900

    chore: adding type hints

--- src/api/endpoints/challenge/utils.py
+++ src/api/endpoints/challenge/utils.py
@@ -1,20 +1,24 @@
 import requests
+from typing import List
 
 from api.config import config
 from api.logger import logger
 
 
-
-def sync_device_with_proxy(request_id, proxy_base_url, browser_devices):
+def sync_device_with_proxy(
+    request_id: str, proxy_base_url: str, browser_devices: List[dict]
+):
     for device_info in browser_devices:
         device_cfg = device_info["device_cfg"]
         order_id = device_info["order_id"]
         try:
             requests.post(
-                    f"{proxy_base_url}/set_device_session",
-                    json={"device_id": device_cfg.id, "order_id": order_id},
-                    headers={"X-API-Key": config.challenge.api_key.get_secret_value()},
-                    timeout=10,
-                ).raise_for_status()
+                f"{proxy_base_url}/set_device_session",
+                json={"device_id": device_cfg.id, "order_id": order_id},
+                headers={"X-API-Key": config.challenge.api_key.get_secret_value()},
+                timeout=10,
+            ).raise_for_status()
         except Exception as e:
-            logger.warning(f"[{request_id}] - Failed to set proxy session for order {order_id}: {e}")
+            logger.warning(
+                f"[{request_id}] - Failed to set proxy session for order {order_id}: {e}"
+            )
